9e4903ab14582f814bd402986c9fa9c32d60af88
llavner <marcus.renvall@gmail.com>
2024-09-13 15:16:27 +0200

    Add project files.

---
+++ Labb01 - Algoritmer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Labb01___Algoritmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter text: ");
+            string userString = "12341567p83812453"; //Console.ReadLine();
+
+            int equalIndex = 0;
+            ulong sumOfAllSubstrings = 0;
+
+            for (int i = 0; i < userString.Length; i++)
+            {
+                //equalIndex = userString.IndexOf(userString);
+
+                Console.Write(userString[i]);
+            }
+        }
+    }
+}
