25498581ce939f3b46e3191361f2cd9dcc05e8c7
llavner <marcus.renvall@gmail.com>
2024-09-13 15:56:41 +0200

    Added IndexOf and checking substrings with IsDigit.

--- Labb01 - Algoritmer/Program.cs
+++ Labb01 - Algoritmer/Program.cs
@@ -1,21 +1,46 @@
-﻿namespace Labb01___Algoritmer
+﻿using System;
+
+namespace Labb01___Algoritmer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Console.Write("Enter text: ");
-            string userString = "12341567p83812453"; //Console.ReadLine();
+            string userString = "29535123p48723487597645723645"; //Console.ReadLine();
 
             int equalIndex = 0;
             ulong sumOfAllSubstrings = 0;
 
+
             for (int i = 0; i < userString.Length; i++)
             {
-                //equalIndex = userString.IndexOf(userString);
+                
+                equalIndex = userString.IndexOf(userString[i], i + 1);
+                string subString = string.Empty;
 
-                Console.Write(userString[i]);
+
+                for (int j = i; j <= equalIndex; j++)
+                {
+
+                    if (char.IsDigit(userString[j]))
+                        subString += userString[j];
+                    else
+                    {
+                        subString = string.Empty;
+                        break;
+                    }
+                    
+                }
+                if (subString == string.Empty)
+                    continue;
+
+                sumOfAllSubstrings += ulong.Parse(subString);
+                
+
             }
+
+            //Console.WriteLine(sumOfAllSubstrings);
         }
     }
 }
